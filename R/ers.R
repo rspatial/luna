92d8c9481b516ee70a7b82a6c2ca649832805c92
Alex Mandel <aimandel@ucdavis.edu>
2019-08-09 14:05:15 -0700

    Not sure if ers login is working anymore, which prevents testing of the cookie passing to a download request.

--- R/ers.R
+++ R/ers.R
@@ -1,9 +1,9 @@
 library(httr)
 library(xml2)
 
-LOGIN_URL <- "https://ers.cr.usgs.gov/login"
+LOGIN_URL <- "https://ers.cr.usgs.gov"
 LOGOUT_URL <- "https://ers.cr.usgs.gov/logout"
-HANDLE <- "https://usgs.gov"
+HANDLE <- httr::handle("https://usgs.gov")
 
 .verify <- function(response){
   # Verify a response by saving the html so a person can look at it
@@ -13,7 +13,9 @@ HANDLE <- "https://usgs.gov"
 
 find_token <- function(){
   # Scrape a csrf token from the login page to allow login
-  response <- httr::GET(LOGIN_URL, verbose())
+  
+  #TODO: not reuse an existing session, since we need a fresh csrf token
+  response <- httr::GET("https://ers.cr.usgs.gov", verbose(), config(forbid_reuse = 1))
 
   if (response$status_code == 200){
     # Search the response content for the csrf
@@ -31,18 +33,20 @@ find_token <- function(){
 
 login_ers <- function(user, passw, csrf){
   # Login to USGS ERS system and return an authentication cookie
+  csrf <- find_token()
+  
   params <- list(
     username = user,
     password = passw,
-    csrf_token = find_token()
+    csrf_token = csrf
   )
   response <- httr::POST(LOGIN_URL, body=params, encode="form", verbose(info=TRUE, ssl=TRUE), handle = HANDLE)
-
+  #response <- httr::POST(LOGIN_URL, body=params, handle = HANDLE, verbose(info=TRUE, ssl=TRUE))
 }
 
 logout_ers <- function(){
   # Logout function
-  response <- httr::GET(LOGIN_URL, verbose(), handle = HANDLE)
+  response <- httr::GET(LOGOUT_URL, verbose(), handle = HANDLE)
 }
 
 find_url <- function(scene){
@@ -51,7 +55,7 @@ find_url <- function(scene){
 
 get_files <- function(scene_url){
   # Download a scene from Earth Explorer
-  # How to reuse the session cookie?
+  # How to reuse the session cookie? set a handle?
   scene_url <- "https://earthexplorer.usgs.gov/download/14320/LC08_CU_002008_20190503_C01_V01/BT/EE"
   response <- httr::GET(scene_url, httr::progress(), httr::write_disk("/tmp/LC08api2.tar"), verbose(), handle = HANDLE)
   
