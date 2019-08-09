c6d6128d7d9202ddd5e98e1a1378b4858c4f9bf9
Alex Mandel <aimandel@ucdavis.edu>
2019-08-08 17:06:15 -0700

    I think the ers login via web page works, might be temporarily blocked from too many attempts. Auth Cookie should stick around for next connection on same url, but what if it shifts, should we set the cookie?

--- R/ers.R
+++ R/ers.R
@@ -5,14 +5,14 @@ LOGIN_URL <- "https://ers.cr.usgs.gov/login"
 
 find_token <- function(){
   # Scrape a csrf token from the login page to allow login
-  response <- httr::GET(LOGIN_URL)
+  response <- httr::GET(LOGIN_URL, verbose())
 
   if (response$status_code == 200){
     # Search the response content for the csrf
     contents <- content(response, as = "parsed", type = "text/html")
     inputs <- xml2::xml_find_all(contents, "//input")
-    token <- xml_attr(inputs[grep("csrf_token", inputs)], "value")
-    return(token)
+    csrf <- xml_attr(inputs[grep("csrf_token", inputs)], "value")
+    return(csrf)
     
   } else {
     print("Web Page not working try again later")
@@ -21,22 +21,27 @@ find_token <- function(){
 
 }
 
-login_ers <- function(user, passw, token){
+login_ers <- function(user, passw, csrf){
   # Login to USGS ERS system and return an authentication cookie
   params <- list(
     username = user,
     password = passw,
-    csrf_token = token
+    csrf_token = find_token()
   )
-  #reponse <- httr::POST(LOGIN_URL, data=params, verbose())
-  reponse <- httr::POST(LOGIN_URL, data=params, verbose())
+  response <- httr::POST(LOGIN_URL, body=params, encode="form", verbose(info=TRUE, ssl=TRUE))
+  
+  # Verify
+  #xml2::write_html( content(response, as="parsed"), "local_test/check.html")
   
 }
 
-find_url <- function(){
+find_url <- function(scene){
   # Scrape the Download URL for the actual urls to files
 }
 
-get_files <- function(){
+get_files <- function(scene_url){
   # Download a scene from Earth Explorer
+  scene_url <- "https://earthexplorer.usgs.gov/download/14320/LC08_CU_002008_20190503_C01_V01/BT/EE"
+  response <- httr::GET(scene_url, httr::progress(), httr::write_disk("/tmp/LC08api.tar"))
+  
 }
