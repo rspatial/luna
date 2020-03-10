db15a6944c3a4a0205fc43ca0019ba764ab5d572
Robert+1-at-925791393128 <r.hijmans@gmail.com>
2020-03-09 19:29:29 -0700

    qc

--- R/modis_qc.R
+++ R/modis_qc.R
@@ -3,7 +3,7 @@
 # Version 0.1
 # Licence GPL v3
 
-unpack_reject <- function(val, bits=32, se, reject) {
+..unpack_reject <- function(val, bits=32, se, reject) {
 	v <- rep(1, length(val))
 	for (i in 1:length(val)) {
 		bin_value <- rev(as.integer(intToBits(val[i]))[1:bits])
@@ -19,15 +19,46 @@ unpack_reject <- function(val, bits=32, se, reject) {
 }
 
 
-modis_mask <- function(x, bits, se, reject, ...) {
+.unpack_reject <- function(val, bits=32, qmat) {
+	from <- as.integer(as.character(qmat[,1]))
+	to <- as.integer(as.character(qmat[,2]))
+	stopifnot((min(from) > 0) & (max(from) <= bits))
+	stopifnot((min(to) > 0) & (max(to) <= bits))
+	stopifnot(all(to >= from))
+
+	rej <- as.character(qmat[,3])
+	reject <- lapply(rej, function(i) unlist(strsplit(i, ",")))
+
+	n <- 1+to-from
+	test <- sapply(1:length(reject), function(i) any(nchar(unlist(reject[i])) != n[i]))
+
+	if (any(test)) {
+		stop(paste("length of 'reject' does not match number of bytes in row(s):", paste(which(test), collapse=",")))
+	}
+	
+
+	v <- rep(1, length(val))
+	for (i in 1:length(val)) {
+		bin_value <- rev(as.integer(intToBits(val[i]))[1:bits])
+		for (j in 1:nrow(qmat)) {
+			x <- paste(bin_value[from[j]:to[j]], collapse="")
+			if (x %in% reject[[j]]) {
+				v[i] <- NA
+				break
+			}
+		}
+	}
+	return (v)
+}
+
+modis_mask <- function(x, bits, qmat, ...) {
 # create a quality mask
 # x     : SpatRaster quality band
 # bits  : number of bits (16 or 32?)
-# se    : matrix with start and end of the bits considered
-# reject: list with the values to be rejected; matching the rows in se
+# qmat   : matrix with start and end of the bits considered and the values to be rejected
 
 	u <- terra::unique(x)
-	v <- unpack_reject(u, bits, se, reject)
+	v <- .unpack_reject(u, bits, qmat)
 	uv <- cbind(u,v)
 	terra::classify(x, uv, ...)
 }
